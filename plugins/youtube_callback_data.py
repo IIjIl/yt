e40a8108268bea18a043a955aca3b2823cc973b4
IIjIl <102681170+IIjIl@users.noreply.github.com>
2022-04-10 08:24:23 +0300

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -25,11 +25,11 @@ async def catch_youtube_fmtid(c, m):
         print(media_type)
         if media_type == 'audio':
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "اغـنية 🎵", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("مـلف 🗂",
+                "ملف صوتي 📼", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("مـلف 🗂",
                                                                                                     callback_data=f"docaudio||{format_id}||{yturl}")]])
         else:
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "فيـديو 🎬", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("مـلف 🗂",
+                "مقطع فيديو 🎞", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("مـلف 🗂",
                                                                                                     callback_data=f"docvideo||{format_id}||{yturl}")]])
 
         await m.edit_message_reply_markup(buttons)
@@ -74,7 +74,7 @@ async def catch_youtube_dldata(c, q):
     if not os.path.isdir(userdir):
         os.makedirs(userdir)
     await q.edit_message_reply_markup(
-        InlineKeyboardMarkup([[InlineKeyboardButton("يتم التـحميل انـتظر ⚕️", callback_data="down")]]))
+        InlineKeyboardMarkup([[InlineKeyboardButton("يتم التحميل انتظر . .", callback_data="down")]]))
     filepath = os.path.join(userdir, filext)
     # await q.edit_message_reply_markup([[InlineKeyboardButton("Processing..")]])
 
@@ -149,7 +149,7 @@ async def send_file(c, q, med, filename):
     print(med)
     try:
         await q.edit_message_reply_markup(
-            InlineKeyboardMarkup([[InlineKeyboardButton("يتـم الـرفع ⚕️", callback_data="down")]]))
+            InlineKeyboardMarkup([[InlineKeyboardButton("جار التحميل . .", callback_data="down")]]))
         await c.send_chat_action(chat_id=q.message.chat.id, action="upload_document")
         # this one is not working
         await q.edit_message_media(media=med)
